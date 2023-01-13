418246d146183114ccd0f7cdb231c37ab7337ef5
Deb Harding <debmh.teach@gmail.com>
2023-01-13 16:59:44 +0000

    Add devise user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "projects#index"
+  devise_for :users
+  root "home#index"
   resources :projects
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
